9169436492fd6e08dec7d86a2d1a706079b219a2
Ernest Joseph <smartlabstech@yahoo.com>
2021-06-15 12:04:04 +0300

    page view layout

--- pages/cases/index.vue
+++ pages/cases/index.vue
@@ -1,5 +1,12 @@
 <template>
-    
+    <div>
+        <div class="breadcrumb">
+             <router-link to="/">Dashboard</router-link>
+            <router-link to="/cases" class="active">Cases</router-link>
+        </div>
+
+        <v-card class="mx-auto defaulf"></v-card>
+    </div>
 </template>
 
 <script>
