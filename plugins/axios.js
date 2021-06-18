23cb5a22d826549c3e9d1013e2a5ebf92b7c4222
Ernest Joseph <smartlabstech@yahoo.com>
2021-06-19 01:57:41 +0300

    plugins added

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,72 +1,35 @@
-import Vue from 'vue';
-import VueToasted from 'vue-toasted';
-Vue.use(VueToasted, {
-  iconPack: 'mdi' // set your iconPack, defaults to material. material|fontawesome|custom-class
-})
+export default function ({
+  $axios,
+  redirect
+}, inject) {
 
-
-export default function ({ $axios, redirect, store }, inject) {
-  $axios.setHeader('Content-Type', 'application/json');
-  //$axios.setToken(store.getters.accessToken, 'Bearer');
-
-
+  // Create a custom axios instance
   const api = $axios.create({
+
     headers: {
       common: {
-        Accept: "application/json, text/plain, */*",
-        'Content-Type': 'application/json'
+        'Accept': 'text/plain, */*',
       },
-    },
+
+    }
   });
-
   api.onRequest(config => {
-    var token = store.getters.accessToken;
-    api.setHeader("Authorization", "Bearer  " + token);
-    api.setHeader("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
-    api.setHeader("Access-Control-Allow-Origin", "*");
-
+    if (localStorage.getItem('qAccessToken') != null) {
+      api.setHeader('Authorization', 'Token ' + localStorage.getItem('qAccessToken'));
+    }
+    console.log('Making request to ' + config.url);
   });
 
   api.onError(error => {
-    const message = error.response.data.defaultUserMessage
-    const code = parseInt(error.response && error.response.status)
-
-  })
-  api.onResponse(response => {
-    // Vue.toasted.show('Success ', { icon: 'check-circle', type: 'success' });
-  });
-  api.onResponseError(error => {
-    const code = parseInt(error.response && error.response.status)
-    if (code === 404) {
-      const message = error.response.data.defaultUserMessage
-      Vue.toasted.error(`Failed with message: ${message}`, {
-        icon: 'close-circle', position: 'top-center', keepOnHover: true, type: 'error',
-        theme: 'outline', duration: 5000
-      });
-    }
-    if (code === 401) {
-      const message = error.response.data.defaultUserMessage;
-      Vue.toasted.error(`${message}`, {
-        icon: 'close-circle', position: 'top-center', keepOnHover: true, type: 'info',
-        theme: 'outline', duration: 5000
-      });
+    const code = parseInt(error.response && error.response.status);
+    if (code === 400) {
+      redirect('/400');
     }
   });
 
-  // Set baseURL to something different
-  // eslint-disable-next-line no-console
-  console.log(
-    process.env.NODE_ENV === "production"
-      ? process.env.baseUrl
-      : process.env.localUrl
-  );
-  /** For UI developers with no local API **/
-  /* api.setBaseURL(  process.env.baseUrl ); */
+  console.log(process.env.NODE_ENV);
+  api.setBaseURL(process.env.NODE_ENV === "development" ? process.env.releaseUrl : process.env.releaseUrl);
 
-
-  /**For production */
-  api.setBaseURL(process.env.NODE_ENV === "production" ? process.env.baseUrl : process.env.localUrl);
   // Inject to context as $api
-  inject("api", api);
+  inject('api', api);
 }
-
