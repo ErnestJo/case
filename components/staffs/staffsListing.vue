77d0e71ca9f5569e5059a61032ed8150e6ffa56b
Ernest Joseph <smartlabstech@yahoo.com>
2021-06-20 13:34:57 +0300

    staff Listing

---
+++ components/staffs/staffsListing.vue
@@ -0,0 +1,138 @@
+<template> 
+    <v-card>
+        <v-data-table
+            dense
+            class="default"
+            :headers="headers"
+            :items="datalist"
+            :search="search"
+            :options="body.options"
+            sort-by="role"
+            :sort-desc="sortDesc"
+            mobile-breakpoint="100"
+            @update:page="updatePagination"
+        >
+        <template v-slot:top>
+
+            <v-toolbar flat class="primary" dark>
+          <v-text-field
+            v-model="search"
+            append-icon="cloud-search"
+            label="Enter search text ..."
+            dense
+            single-line
+            hide-details
+          ></v-text-field>
+
+          <v-spacer></v-spacer>
+          <div >
+             <v-btn medium  class="button" to="/iReport/add"
+              ><v-icon>mdi-plus</v-icon
+              > Create Ir</v-btn>
+          </div>
+        </v-toolbar>
+        </template>
+       <template v-slot:[`item.gender`]="{ item }">
+        <v-tooltip top v-if="item.gender == 'Female'" color="green">
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon v-bind="attrs" v-on="on" small color="green darken-2"
+              >mdi-gender-female
+            </v-icon>
+          </template>
+          <span>{{ $t("label.tooltip.genderfemale") }}</span>
+        </v-tooltip>
+        <v-tooltip top v-if="item.gender == 'Male'" color="primary">
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon v-bind="attrs" v-on="on" small color="primary darken-2"
+              >mdi-gender-male
+            </v-icon>
+          </template>
+          <span>{{ $t("label.tooltip.gendermale") }}</span>
+        </v-tooltip>
+        <v-tooltip top v-if="item.gender == 'Unspecified'" color="cyan">
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon v-bind="attrs" v-on="on" small color="cyan darken-2"
+              >mdi-gender-male-female
+            </v-icon>
+          </template>
+          <span>{{ $t("label.tooltip.genderother") }}</span>
+        </v-tooltip>
+      </template>
+          <template v-slot: [`item.status`] = "{ item }">
+              
+          </template>
+
+        </v-data-table>
+    </v-card>
+</template>
+
+<script>
+export default {
+     props: ["datalist", "pagetitle"],
+     data: () => ({
+        dialog: false,
+        search: "",
+        sortDesc: false,  
+         headers: [
+             { text: "Title", value: "title" },
+      { text: "Staff Name", value: "name" },
+      { text: "Staff Work No", value: "" }, 
+      { text: "Role", value: "role" },
+      { text: "Email", value: "email" },
+    
+    ],
+    desserts: [], 
+     body: {
+      options: {
+        page: 1,
+        sortBy: ["name"]
+      }
+    }
+    
+}),
+
+    computed: {
+    isAppointmentRoute: {
+      get() {
+        // get route of cases
+      }
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+  
+  created() {},
+
+ methods: {
+   
+    editItem(item) {
+      this.editedIndex = this.datalist.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+      this.editedItemId = item.id;
+    },
+
+    handleClick: function(value) {
+    //   this.$router.push("/patients/" + value.id);
+    },
+    initialize() {},
+    updatePagination: function(val) {
+      console.log(val);
+    }
+  },
+
+  beforeMount() {
+    // this.$store.dispatch("retrievepatients");
+  }
+}
+</script>
+
+<style scoped>
+.v-text-field__slot {
+  width: 20px;
+}
+</style>
