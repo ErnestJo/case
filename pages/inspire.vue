a81cb1946ccc21d5bf546752e74efd4cf69eb75d
Ernest Joseph <smartlabstech@yahoo.com>
2021-06-24 22:47:56 +0300

    info card

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,62 +1,52 @@
 <template>
-  <v-container fluid>
-    <!-- <v-progress-circular
-     
-      indeterminate
-      color="grey"
-      size="24"
-    ></v-progress-circular> -->
-    <div >
-      <v-data-table
-        dense
-        class="default"
-        :headers="headers"
-        :search="search"
-        mobile-breakpoint="100"
-        @click:row="handleClick"
-        @update:page="updatePagination"
-      >
-      </v-data-table>
-    </div>
+  <v-container fill-height fluid grid-list-xl>
+    <v-row>
+      <v-col xs="12" md="3" sm="12" >
+        <v-card flat>
+         <v-img
+             :src="filebaseUri + entityThumbNail"
+            lazy-src="https://www.attendanceworks.org/wp-content/uploads/2020/09/img-placeholder.png"
+            aspect-ratio="1"
+            class="grey lighten-2 align-end"
+            height="200px"
+            max-height="200px"
+         >
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+            <v-card-title>
+              
+            </v-card-title>
+         </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+
   </v-container>
 </template>
-<script>
 
-import ServiceCard from "@/components/profile/consultation_card";
+<script>
 export default {
-  components: {
-    "service-card": ServiceCard
-  },
-  props: {
-    services: {
-      type: Array,
-      default: null
-    }
-  },
-  data: () => ({
-    search: null,
-    headers: [
-      { text: "Accuser Name", value: "name" },
-      { text: "Accuser Age", value: "age" },
-      { text: "Accuser Phone", value: "accuserName", sortable: false },
-      { text: "Accuser Occupation", value: "accuserOccupation" },
-      { text: "Accuser Nationality ", value: "accuserNationlity" },
-    ],
-    body: {
-      options: {
-        page: 1,
-        itemsPerPage: 5,
-        sortBy: ["accuserName"]
-      }
-    }
-  }),
-//   methods: {
-//     handleClick: function(value) {
-//       this.$router.push("/consultations/" + value.id);
-//     },
-//     updatePagination: function(val) {
-//       console.log(val);
-//     }
-//   }
-};
+
+}
 </script>
+
+<style>
+.basil {
+  background-color: #fffbe6 !important;
+}
+
+.basil--text {
+  color: #356859 !important;
+}
+.v-window,
+.v-item-group .v-window__container,
+.v-window-item {
+  width: 100%;
+}
+</style>
