7ed10b5b6cecec1ab859839656bec7bf23964667
Ernest Joseph <smartlabstech@yahoo.com>
2021-07-02 18:46:15 +0300

    pulling Data in  ireport Tabs

--- components/profile/tabs/iReport.vue
+++ components/profile/tabs/iReport.vue
@@ -1,34 +1,35 @@
 <template>
   <v-container fluid>
-    <!-- <v-progress-circular
-     
+    <v-progress-circular
+      v-if="repo == null"
       indeterminate
       color="grey"
       size="24"
-    ></v-progress-circular> -->
-    <div >
+    ></v-progress-circular>
+    <div v-else>
       <v-data-table
         dense
         class="default"
         :headers="headers"
+        :items="repo"
         :search="search"
         mobile-breakpoint="100"
         @click:row="handleClick"
         @update:page="updatePagination"
       >
+        
       </v-data-table>
     </div>
   </v-container>
 </template>
 <script>
-
 import ServiceCard from "@/components/profile/consultation_card";
 export default {
   components: {
     "service-card": ServiceCard
   },
   props: {
-    services: {
+    repo: {
       type: Array,
       default: null
     }
@@ -36,26 +37,27 @@ export default {
   data: () => ({
     search: null,
     headers: [
-      { text: "Report Number ", value: "name" },
-      { text: "Category of Offence", value: "age" },
-      { text: "Offence", value: "accuserName", sortable: false },
-      { text: "Section", value: "accuserOccupation" },
+      { text: "Ir number", value: "irNumber" },
+      { text: "Category ", value: "categoryOfOffence" },
+      { text: "Offence", value: "offence", sortable: false },
+      { text: " Section", value: "section" },
+      { text: "Remark", value: "remark" },
+      
     ],
     body: {
       options: {
         page: 1,
         itemsPerPage: 5,
-        sortBy: ["Report Number"]
       }
     }
   }),
-//   methods: {
-//     handleClick: function(value) {
-//       this.$router.push("/consultations/" + value.id);
-//     },
-//     updatePagination: function(val) {
-//       console.log(val);
-//     }
-//   }
+  methods: {
+    handleClick: function(value) {
+      this.$router.push("api/v1/investigationReports/" + value.id);
+    },
+    updatePagination: function(val) {
+      console.log(val);
+    }
+  }
 };
 </script>
