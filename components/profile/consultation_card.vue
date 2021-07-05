557e8ff0e02c86db39673f64b4f99659f29c5182
Ernest Joseph <smartlabstech@yahoo.com>
2021-07-05 09:23:05 +0300

    to assign works

--- components/profile/consultation_card.vue
+++ components/profile/consultation_card.vue
@@ -1,6 +1,5 @@
 <template>
   <v-card class="mx-auto default "> 
-
      <v-progress-circular
       v-if="services == null"
       indeterminate
@@ -14,25 +13,73 @@
         :items="services"
         :search="search"
         width="100%"
-        @click:row="handleClick"
+        @click:row="dialog"
         @update:page="updatePagination"
         >
-        <template v-slot:top>
-          <v-toolbar flat color="primary" dark>
-            <h2>
-              <strong> {{title}}</strong>
-            </h2>
-            <v-spacer></v-spacer>
-            <v-text-field
-              v-model="search"
-              append-icon="search"
-              label="Enter search text ..."
-              dense
-              single-line
-              hide-details
-            ></v-text-field>
-          </v-toolbar>
-        </template>
+          <template v-slot:top>
+        <v-toolbar flat color="primary" dark>
+          <v-toolbar-title>
+            </v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="700px">
+            
+            <v-card>
+              <v-card-title class="primary">
+                <span></span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-form ref="form" v-model="valid" lazy-validation>
+                    <v-row>
+                      <v-col cols="12" sm="12" md="4">
+                        <v-checkbox
+                          v-model="editedItem.isAssigned"
+                          label="Any thing was Stolen?"
+                          ></v-checkbox>
+                      </v-col>
+                      <v-col cols="12" sm="12" md="8">
+                        <v-select
+                          
+                          chips
+                          disable-lookup
+                          deletable-chips
+                          clearable
+                          small-chips
+                          multiple
+                          :rules="[
+                            v => !!v || 'You must select one to continue!'
+                          ]"
+                          label="Select role officers to be incharge"
+                          required
+                        ></v-select>
+                      </v-col>
+                    </v-row>
+                  </v-form>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" small @click="close">
+                 cancel
+                </v-btn>
+                <v-btn color="warning" small @click="save">
+                  save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <td @click.stop>
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+        </td>
+      </template>
       </v-data-table>
     </div>
   </v-card>
@@ -54,13 +101,31 @@ export default {
   },
    data: () => ({
     search: null,
+     dialog: false,
     headers: [
       { text: "Case Number", value: "caseNumber", sortable: false },
       { text: "Investigator", value: "assignTo" },
       { text: "Status", value: "status" },
+      { text: "Actions", value: "actions", sortable: false } 
     ],
+   editedItem: {
+      id: "",
+      isAssigned: false ,
+      assignTo: ""
+    },
+
+    defaultItem: {
+      id: "",
+     isAssigned: false,
+      assignTo: ""
+
+    },
+
+      dialog: false,
+      valid: true,
+      
     body: {
-      options: {
+        options: {
         page: 1,
         itemsPerPage: 10,
         sortBy: ["assignTo"]
@@ -68,9 +133,47 @@ export default {
     }
    }),
   methods: {
-    handleClick: function(value) {
-      this.$router.push("/cases/" + value.id);
+
+       handleClick: function(item) {
+      this.$router.push("roles/" + item.id);
     },
+
+
+        close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+      
+    },
+     request_data() {
+      // this.$store.dispatch("request_role_privileges");
+      // this.$store.dispatch("fetchuserroles");
+    },
+    editItem(item) {
+      this.editedIndex = this.services.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      // var roles = [];
+      // this.editedItem.roles.forEach(role => {
+      //   roles.push(role.id);
+      // });
+      // this.editedItem.roles = roles;
+      this.dialog = true;
+    },
+
+        save() {
+      if (this.editedIndex > -1) {
+        console.log(this.editedItem);
+        var did = this.editedItem.id;
+        var payload = { id: did, data: this.editedItem };
+        this.$store.dispatch("updateassignee", payload);
+      } else {
+       this.close();
+      }
+     
+    },
+    
     updatePagination: function(val) {
       console.log(val);
     }
@@ -79,5 +182,9 @@ export default {
 </script>
 
 <style>
-
+h2 {
+  text-align: center;
+  margin-top: 100px;
+  font-family: sans-serif;
+}
 </style>
