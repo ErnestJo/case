efba2d7ef4cdebda8a6b3dd4090bde2301fbe1bf
Ernest Joseph <smartlabstech@yahoo.com>
2021-07-08 01:16:37 +0300

    my cases

---
+++ pages/investigation/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div class="breadcrumb">
+             <router-link to="/">Dashboard</router-link>
+            <router-link to="/cases" class="active">Cases</router-link>
+        </div>
+
+        <v-card class="mx-auto defaulf">
+            <case-list
+            :datalist="casesi"
+            :pagetitle="all_pagetitle"
+             >    
+            </case-list>
+        </v-card>
+    </div>
+</template>
+
+<script>
+
+import casesListing from "@/components/cases/casesListing";
+
+export default {
+
+    components:{
+        "case-list": casesListing
+    },
+
+    data: () => ({
+        dialog: false,
+        search: "",
+        all_pagetitle: "All Cases"
+    }),
+    
+ created(){
+    this.$store.dispatch("retrieve_mycases");
+   
+  },
+    computed: {
+        formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item"; 
+        },
+        
+    casesi (){
+    return this.$store.getters.listmycases;
+    }
+    
+ },
+
+  beforeMount() {
+    // store inatakiwa hapa data
+  }
+
+}
+</script>
+
+<style>
+
+</style>
